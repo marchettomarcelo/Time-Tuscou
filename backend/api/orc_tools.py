824d517090a1e2cad5cdaeca64fc3229d249d02d
FeMCDias <femcdias@gmail.com>
2022-10-17 11:02:13 -0300

    consertamos tesseract PARA MAC

--- backend/api/orc_tools.py
+++ backend/api/orc_tools.py
@@ -1,11 +1,20 @@
+import pytesseract
 from PIL import Image
-from pytesseract import pytesseract
-import cv2
+
+
+# pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+# image = Image.open('../static/media/extrato/extrato_itau.jpeg')
+# texto = pytesseract.image_to_string(image, lang='por', config='--psm 6')
+# print(float(texto.splitlines()[10].split(' ')[-1].replace('.', '').replace(',', '.')))
 
 def orc(path):
+    pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
     context = {}
     image = Image.open(path)
     image_text = pytesseract.image_to_string(image, lang='por', config='--psm 6')
     context.update({'image_text': image_text})
+    print(context)
     #TODO: ORC - read file and create transactions
     return context
+
+orc('../../static/media/extrato/extrato_itau.jpeg')
